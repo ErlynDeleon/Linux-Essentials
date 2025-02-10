82f8ec0b3dbd85a60ae7042a054bf5246dba8b26
ErlynDeleon <152839946+ErlynDeleon@users.noreply.github.com>
2025-02-10 06:13:01 +0000

    done

---
+++ MYSCRIPT.sh
@@ -0,0 +1,15 @@
+# echo "Hello Juan!"
+
+# read name
+# echo "Your name is $name"
+
+#!bin/bash/
+
+#echo "Enter your favorite color:"
+#read color
+#echo "Your favorite color is $color!"
+
+read name
+read favoritefood
+
+echo "Hello $name! I see you love $favoritefood!"
